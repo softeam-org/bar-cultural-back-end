47873569a791f7758c701c66af120707f71bff57
0vitor <w.wutaes@gmail.com>
2024-02-08 10:09:13 -0300

    feat: create administrators module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AdministratorsModule } from './administrators/administrators.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [AdministratorsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
