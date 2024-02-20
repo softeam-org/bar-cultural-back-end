86fef398674e6830534538b198b00dd479eaf3c6
FernandaKaroliny <fernandaks2020@outlook.com>
2024-02-20 07:55:35 -0300

    feat: add products module to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AdministratorsModule } from './administrators/administrators.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AdministratorsModule],
+  imports: [AdministratorsModule, ProductsModule],
   controllers: [],
   providers: [PrismaService],
 })
