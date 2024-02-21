fa86b10aa1d203d585b17c181f0b42998c7f5eb0
k1llgray <hermesjgcode@gmail.com>
2024-02-21 09:55:25 -0300

    Fix: Fix merge conflict in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { AdministratorsModule } from './administrators/administrators.module';
-import { EventModule } from './events/event.module';
 import { CategoriesModule } from './categories/categories.module';
+import { EventModule } from './events/event.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
