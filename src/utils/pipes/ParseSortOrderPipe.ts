4b639588e35d71c8fc7a1e4475bc50412cb4fcf0
0vitor <w.wutaes@gmail.com>
2024-02-23 22:03:10 -0300

    feat: create pipe to validate SortOrder

---
+++ src/utils/pipes/ParseSortOrderPipe.ts
@@ -0,0 +1,13 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+
+import { SortOrder } from '../types/SortOrder';
+
+@Injectable()
+export class ParseSortOrderPipe implements PipeTransform<string, SortOrder> {
+  transform(value: string): SortOrder {
+    if (value !== 'asc' && value !== 'desc') {
+      throw new BadRequestException("Ordem só pode ser 'asc' ou 'desc'");
+    }
+    return value as SortOrder;
+  }
+}
